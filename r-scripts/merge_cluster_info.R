6911b0a99dcb34e8a5af9db0e8d9f1c87a785b40
Cindy Huang <huang.cindy@hotmail.com>
2022-11-20 12:12:35 -0500

    add script for merging cluster info

---
+++ r-scripts/merge_cluster_info.R
@@ -0,0 +1,60 @@
+### For merging in cluster information
+repo.dir = '/data/home/huan1766/PM25-Fire/'
+
+## Capture messages and errors to a file.
+zz <- file(paste0(repo.dir, "errors-logs/errors_merge_cl.Rout"), open="wt")
+sink(zz, type="message")
+
+pkgs = c('tidyverse', 'dplyr', 'sf', 'units')
+for(p in pkgs) require(p, character.only = T)
+rm(p, pkgs)
+
+# Specify directory
+data.fire.dir = paste0(repo.dir, 'data/fire/')
+data.smoke.dir = paste0(repo.dir, 'data/smoke/')
+
+# Convert dataframes to sf objects
+years <- seq("2000", "2021", by=1)
+months <- seq("01", "12", by=1)
+months[1:9] <- paste0("0",months[1:9])
+
+message("==========START READING==========")
+aqs <- read.delim(paste0(repo.dir, "data/merged/Merged_AQS_PM25_2000_2021_Cali.csv"), sep=",", strip.white=T)
+
+message("==========FINISHED READING==========")
+message("Original dataset dimension: ", dim(aqs)[1], " observations and ", dim(aqs)[2], " features.")
+
+message("==========START MERGING==========")
+aqs.annual <- vector("list", length = length(years))
+
+for(i in 1:length(years)){
+  y <- years[i]
+  message("==========PROCESSING: ", y, "==========")
+  year_aqs <- aqs %>%
+    filter(format(as.POSIXct(Date, format="%Y-%m-%d"), format="%Y") == y)
+  cl_filename <- paste0(y, "_fire_cluster_info.shp")
+  year_cl <- st_read(paste0(data.fire.dir, "cluster_info/", y, "/", cl_filename)) %>%
+    st_drop_geometry() %>%
+    dplyr::select(-date)
+  year_aqs <- year_aqs %>%
+    st_drop_geometry() %>%
+    left_join(year_cl, by=c("closest_cl"="cluster"))
+  aqs.annual[[i]] <- year_aqs
+  message("==========FINISHED: ", y, "==========")
+}
+
+aqs.merged <- do.call("rbind", aqs.annual)
+message("==========FINISHED MERGING==========")
+message("Merged dataset dimension: ", dim(aqs.merged)[1], " observations and ", dim(aqs.merged)[2], " features.")
+write.csv(aqs.merged, paste0(repo.dir, "data/merged/WithCL_AQS_PM25_2000_2021_Cali.csv"), row.names = FALSE)
+
+## reset message sink and close the file connection
+sink(type="message")
+close(zz)
+
+## Display the log file
+readLines(paste0(repo.dir, "errors-logs/errors_merge_cl.Rout"))
+    
+    
+    
+    
