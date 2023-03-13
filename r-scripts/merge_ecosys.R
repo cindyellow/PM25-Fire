43048ec49d7d3fb3242cf6ac1cf2faa3f590ae68
Cindy Huang <huang.cindy@hotmail.com>
2023-03-13 16:45:31 -0400

    better package req

--- r-scripts/merge_ecosys.R
+++ r-scripts/merge_ecosys.R
@@ -8,6 +8,8 @@ sink(zz, type="message")
 pkgs = c('dplyr', 'FedData', 'sf', 'raster')
 for(p in pkgs) require(p, character.only = T)
 rm(p, pkgs)
+new.packages <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
 # Specify directory
 data.merged.dir = paste0(repo.dir, 'data/merged/')
